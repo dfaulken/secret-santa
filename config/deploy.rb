c85d9979aa46fa56dd91a2e841f2a072fea988f4
David Faulkenberry <david.faulkenberry@gmail.com>
2016-11-22 09:45:18 -0500

    Add secrets.yml to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :linked_files, fetch(:linked_files, []).push(
   'config/application.yml',
   'config/database.yml'
+  'config/secrets.yml'
 )
 
 set :linked_dirs, fetch(:linked_dirs, []).push(
